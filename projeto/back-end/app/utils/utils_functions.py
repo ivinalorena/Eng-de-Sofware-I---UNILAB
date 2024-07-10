a03fd76aa05fbf46b9e2e82d751cc2570bf92cbe
josedembo <josedembo18@aluno.unilab.edu.br>
2024-07-10 11:50:46 -0300

    feat(beck-end): Atualiza o pacote de funções úteis
    - Adiciona as funções de mapeamentos dos objetos de da classe Produto

--- projeto/back-end/app/utils/utils_functions.py
+++ projeto/back-end/app/utils/utils_functions.py
@@ -1,4 +1,4 @@
-from app.model.models import Usuario
+from app.model.models import Usuario, Produto
 
 def get_user_dict(user: Usuario) -> dict:
     item = {}
@@ -24,3 +24,20 @@ def get_all_users_dict(users: list[Usuario]) -> list[dict]:
         item["updated_at"] = user.updated_at if user.updated_at else "not updated yet"
         users_dict.append(item)
     return users_dict
+
+def get_product_dict(produto: Produto) -> dict:
+    item = {}
+    item["id"] = produto.id
+    item["nome"] = produto.nome
+    item["preco"] = produto.preco
+    item["restaurante_id"] = produto.restaurante_id
+    item["descricao"] = None if not produto.descricao else produto.descricao
+    item["created_at"] = produto.created_at
+    item["updated_at"] = produto.updated_at if produto.updated_at else "not updated yet"
+    return item
+
+def get_list_product_dict(produtos: list[Produto]) -> list[dict]:
+    produtos_list = []
+    for produto in produtos:
+        produtos_list.append(get_product_dict(produto))
+    return produtos_list
