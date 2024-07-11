3123ca5f6205ba44772e33013408b802a19e517a
josedembo <josedembo18@aluno.unilab.edu.br>
2024-07-11 13:34:37 -0300

    feat(back-end): Adiciona os blueprints das rotas de pedidos e restaurante

--- projeto/back-end/app/__init__.py
+++ projeto/back-end/app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from app.controller.auth import auth 
 from app.controller.usuario import usuario
 from app.controller.produto import produto
+from app.controller.restaurante import restaurante
+from app.controller.pedido import pedidos
 from app.config import Config
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger, swag_from
@@ -21,6 +23,8 @@ def create_app(test_config=None):
     app.register_blueprint(auth)
     app.register_blueprint(usuario)
     app.register_blueprint(produto)
+    app.register_blueprint(restaurante)
+    app.register_blueprint(pedidos)
     
     Swagger(app=app, config= swagger_config, template=template)
     
