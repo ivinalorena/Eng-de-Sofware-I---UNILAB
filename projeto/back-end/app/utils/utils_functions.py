7d275edbaa39a04a77386494a261d66ede9ab339
josedembo <josedembo18@aluno.unilab.edu.br>
2024-07-11 13:32:55 -0300

    feat(back-end): Atualiza o pacote utils com novas funções

--- projeto/back-end/app/utils/utils_functions.py
+++ projeto/back-end/app/utils/utils_functions.py
@@ -1,4 +1,4 @@
-from app.model.models import Usuario, Produto
+from app.model.models import Usuario, Produto, Restaurante, Pedido
 
 def get_user_dict(user: Usuario) -> dict:
     item = {}
@@ -41,3 +41,35 @@ def get_list_product_dict(produtos: list[Produto]) -> list[dict]:
     for produto in produtos:
         produtos_list.append(get_product_dict(produto))
     return produtos_list
+
+def get_restaurante_dict(restaurante: Restaurante) -> dict:
+    item = {}
+    item["id"] = restaurante.id
+    item["nome"] = restaurante.nome
+    item["classificacao"] = None if not restaurante.classificacao else restaurante.classificacao
+    item["created_at"] = restaurante.created_at
+    item["updated_at"] = restaurante.updated_at if restaurante.updated_at else "not updated yet"
+    # item["produtos"] = [] if not restaurante.produtos else restaurante.produtos
+    return item
+
+def get_list_restaurantes_dict(restaurantes: list[Restaurante]) -> list[dict]:
+    restaurantes_list = []
+    for restaurante in restaurantes:
+        restaurantes_list.append(get_restaurante_dict(restaurante))
+    return restaurantes_list
+
+def get_pedidos_dict(peidio:Pedido ) -> dict:
+    item = {}
+    item["id"] = peidio.id
+    item["usuario_id"] = peidio.usuario_id
+    item["restaurante_id"] = peidio.restaurante_id
+    item["produto_id"] = peidio.produto_id
+    item["created_at"] = peidio.created_at
+    item["updated_at"] = peidio.updated_at if peidio.updated_at else "not updated yet"
+    return item
+
+def get_list_pedidos_dict(pedidos: list[Pedido]) -> list[dict]:
+    pedidos_list = []
+    for pedido in pedidos:
+        pedidos_list.append(get_pedidos_dict(pedido))
+    return pedidos_list
